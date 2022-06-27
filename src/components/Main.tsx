59be78e78d1abd4bf391a4bae1539d347d5c5d5a
Mirko-khatab <mirkokawa7@gmail.com>
2022-06-27 15:47:01 +0300

    create structure of my resume

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='bg-red-400 a4size flex flex-row'>
+     <div className="basis-1/4 max-w-1/2 w-1/2 bg-blue-300">01</div>
+  <div className="basis-1/4 w-full bg-red-200">02</div>
+
+    </div>
+  )
+}
+
+export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import Clintform from '../components/Clintform';
-import Team from '../components/Team';
+import Main from "../components/Main"
 export default function Home() {
   return (
-    <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
-      <main>
-    <Team/>
-    <Clintform/>
-      </main>
+    
+      <div>
+        <Main/>
+  
+    
     </div>
   )
 }
